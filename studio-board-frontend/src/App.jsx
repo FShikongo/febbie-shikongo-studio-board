6474b86c3b1b249a0b4d291560e8d215d262b78c
Febbie Shikongo <feb.shikongo@gmail.com>
2025-02-15 12:59:34 -0500

    Updated Add and Edit Job/Event Card to Item, updated partials folder with styling details

--- studio-board-frontend/src/App.jsx
+++ studio-board-frontend/src/App.jsx
@@ -4,16 +4,24 @@ import "./App.scss";
 import HomePage from "./pages/HomePage/HomePage";
 import DashBoardPage from "./pages/DashBoardPage/DashBoardPage";
 import DetailsPage from "./pages/DetailsPage/DetailsPage";
+import AddJobEventItem from "./component/AddJobEventItem/AddJobEventItem";
+import EditJobEventItem from "./component/EditJobEventItem/EditJobEventItem";
+import Header from "./component/Header/Header";
+import Footer from "./component/Footer/Footer";
 
 function App() {
   return (
     <>
       <BrowserRouter>
+        <Header />
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/dashboard" element={<DashBoardPage />} />
           <Route path="/dashboard/:id" element={<DetailsPage />} />
+          <Route path="/dashboard/add" element={<AddJobEventItem />} />
+          <Route path="/dashboard/:id/edit" element={<EditJobEventItem />} />
         </Routes>
+        <Footer />
       </BrowserRouter>
     </>
   );
