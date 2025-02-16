eb307f4a7e545cd1560b2db08ed3e7cb46f9a965
Febbie Shikongo <feb.shikongo@gmail.com>
2025-02-15 21:13:11 -0500

    Created logo.jsx  component

--- studio-board-frontend/src/component/Logo/Logo.jsx
+++ studio-board-frontend/src/component/Logo/Logo.jsx
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 export default function Logo() {
   return (
     <Link to="/">
-      <img src={logo} className="logo" alt="InStock Logo" />
+      <img src={logo} className="logo" alt="Studio Board Logo" />
     </Link>
   );
 }
