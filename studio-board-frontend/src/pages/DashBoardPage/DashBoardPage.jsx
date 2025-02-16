1e7266b414fd311968c52df37097efeba53de410
Febbie Shikongo <feb.shikongo@gmail.com>
2025-02-15 20:37:50 -0500

    Added imports to dashboard and details page

--- studio-board-frontend/src/pages/DashBoardPage/DashBoardPage.jsx
+++ studio-board-frontend/src/pages/DashBoardPage/DashBoardPage.jsx
@@ -1,35 +1,10 @@
-import "./CoPilotPage.scss";
-import React from "react";
-import axios from "axios";
-import Copilot from "../../components/Copilot/Copilot";
-import Robot from "../../components/Robot/Robot";
-import { useState, useEffect } from "react";
+import "./DashBoardPage.scss";
+import DashBoard from "../../component/DashBoard/DashBoard";
 
-function CoPilotPage() {
-  const [allChats, setAllChats] = useState(null);
-  const API_URL = import.meta.env.VITE_API_URL;
-
-  useEffect(() => {
-    const getChat = async () => {
-      try {
-        const { data } = await axios.get(`${API_URL}/chat-bot`);
-        console.log(data);
-        setAllChats(data);
-      } catch (err) {
-        console.error(err);
-      }
-    };
-    getChat();
-  }, []);
-  if (!allChats) {
-    return <>Loading...</>;
-  }
-
+export default function DashBoard() {
   return (
-    <div className="copilot__page">
-      <Copilot allChats={allChats} />
-      <Robot />
-    </div>
+    <>
+      <DashBoard />
+    </>
   );
 }
-export default CoPilotPage;
