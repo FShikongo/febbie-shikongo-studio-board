d6458f53810c5239dbbe97f4c75dc4cc2bbbc5e5
Febbie Shikongo <feb.shikongo@gmail.com>
2025-02-21 17:54:31 -0500

    Added auto scrolling logos to Partner Showcase and reconfigured the Feature section

--- studio-board-frontend/src/component/Features/Features.jsx
+++ studio-board-frontend/src/component/Features/Features.jsx
@@ -12,18 +12,27 @@ const FeaturedOpportunities = () => {
       title: "Crew Call Short Film",
       description:
         "Join a dynamic team in the film industry. Apply now for an exciting role!",
+      company: "Studio Board Productions",
+      website: "www.studioboard.com",
+      email: "info@studioboard.com",
     },
     {
       image: makeup,
       title: "Hair and Makeup",
       description:
         "Designs and styles of hair and make-up looks for all the characters.",
+      company: "GlamFX Studio",
+      website: "www.glamfxstudio.com",
+      email: "info@glamfxstudio.com",
     },
     {
       image: cannesff,
       title: "Film Festival",
       description:
         "Attend the biggest film festival of the year and network with industry leaders.",
+      company: "Cannes Film Festival",
+      website: "www.festival-cannes.com",
+      email: "info@festival-cannes.com",
     },
   ];
 
@@ -45,6 +54,20 @@ const FeaturedOpportunities = () => {
             <div className="features__text">
               <h3 className="features__title">{opportunity.title}</h3>
               <p className="features__description">{opportunity.description}</p>
+              <p className="features__company">
+                <strong>{opportunity.company}</strong>
+              </p>
+              <p className="features__contact">
+                <a href={`mailto:${opportunity.email}`}>{opportunity.email}</a>{" "}
+                |{" "}
+                <a
+                  href={`https://${opportunity.website}`}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  {opportunity.website}
+                </a>
+              </p>
             </div>
           </div>
         ))}
@@ -54,104 +77,3 @@ const FeaturedOpportunities = () => {
 };
 
 export default FeaturedOpportunities;
-
-// import { useState, useEffect } from "react";
-// import React from "react";
-// import "./Features.scss";
-
-// import bts from "../../assets/Gif/bts.gif";
-// import makeup from "../../assets/Gif/hair-makeup2.gif";
-// import cannesff from "../../assets/Gif/festival-de-cannes.gif";
-
-// const featureVideos = [
-//   { id: "feature1", img: bts },
-//   { id: "feature2", img: makeup },
-//   { id: "feature3", img: cannesff },
-// ];
-
-// const FeaturedOpportunities = () => {
-//   const featuredOpportunities = [
-//     {
-//       image: bts,
-//       title: "Crew Call Short Film",
-//       description:
-//         "Join a dynamic team in the film industry. Apply now for an exciting role!",
-//     },
-//     {
-//       image: makeup,
-//       title: "Hair and Makeup",
-//       description:
-//         "Designs and styles of hair and make-up looks for all the characters.",
-//     },
-//     {
-//       image: cannesff,
-//       title: "Film Festival",
-//       description:
-//         "Attend the biggest film festival of the year and network with industry leaders.",
-//     },
-//   ];
-
-//   return (
-//     <section className="features">
-//       <h2 className="features__title">Featured Opportunities</h2>
-//       <div className="features__list">
-//         {featuredOpportunities.map((opportunity, index) => (
-//           <div key={index} className="features__card">
-//             <div
-//               className="features__card-image"
-//               style={{ backgroundImage: `url(${opportunity.image})` }}
-//             >
-//               <h3 className="features__card-title">{opportunity.title}</h3>
-//             </div>
-//             <div className="features__card-text">
-//               <p className="features__card-description">
-//                 {opportunity.description}
-//               </p>
-//             </div>
-//           </div>
-//         ))}
-//       </div>
-//     </section>
-//   );
-// };
-
-// export default FeaturedOpportunities;
-
-// export default function Features() {
-//   return (
-//     <main className="main">
-//       <section className="features">
-//         <h2 className="features__title">Featured Opportunities</h2>
-//         <div className="features__list">
-//           {featureVideos.map((feature) => (
-//             <div key={feature.id} className="features__card">
-//               <div className="features__card-image">
-//                 <img
-//                   src={feature.img}
-//                   alt={feature.id}
-//                   className="feature-gif"
-//                 />
-//                 <h3 className="features__card-title">
-//                   {feature.id === "feature1"
-//                     ? "Crew Call Short Film"
-//                     : feature.id === "feature2"
-//                     ? "Hair and Makeup"
-//                     : "Film Festival"}
-//                 </h3>
-//               </div>
-//               <div className="features__card-text">
-//                 <p className="features__card-description">
-//                   {feature.id === "feature1"
-//                     ? "Join a dynamic team in the film industry. Apply now for an exciting role!"
-//                     : feature.id === "feature2"
-//                     ? "Designs and styles of hair and make-up looks for all the characters."
-//                     : "Attend the biggest film festival of the year and network with industry leaders."}
-//                 </p>
-//               </div>
-//             </div>
-//           ))}
-//         </div>
-//       </section>
-//     </main>
-//   );
-// }
--- studio-board-frontend/src/component/PartnerShowcase/PartnerShowcase.jsx
+++ studio-board-frontend/src/component/PartnerShowcase/PartnerShowcase.jsx
@@ -20,6 +20,27 @@ import laiff from "../../assets/Logo/LA-International-FF-logo.png";
 import sff from "../../assets/Logo/sundance-film-festival-logo.jpg";
 import tps from "../../assets/Logo/Tyler-Perry-Studios.jpg";
 
+const logos = [
+  biaff,
+  atlantaff,
+  netflix,
+  nciff,
+  cinemap,
+  whistlerff,
+  warnerbros,
+  ffa,
+  sonyp,
+  sidewalkff,
+  disneyp,
+  pp,
+  tiff,
+  nff,
+  own,
+  laiff,
+  sff,
+  tps,
+];
+
 export default function PartnerShowcase() {
   return (
     <section className="partner-showcase">
@@ -29,99 +50,146 @@ export default function PartnerShowcase() {
         renowned media companies and festivals, helping to create valuable job
         opportunities and foster meaningful networking experiences.
       </p>
-      <div className="partner-showcase__logos">
-        <img
-          className="partner-showcase__logo"
-          src={biaff}
-          alt="Berlin International Art Film Festival"
-        />
-        <img
-          className="partner-showcase__logo"
-          src={atlantaff}
-          alt="Atlanta Film Festival"
-        />
-        <img className="partner-showcase__logo" src={netflix} alt="Netflix" />
-        <img
-          className="partner-showcase__logo"
-          src={nciff}
-          alt="Niagara Canada International Film Festival"
-        />
-        <img
-          className="partner-showcase__logo"
-          src={cinemap}
-          alt="Cinema Production"
-        />
-        <img
-          className="partner-showcase__logo"
-          src={whistlerff}
-          alt="Whistler Film Festials"
-        />
-        <img
-          className="partner-showcase__logo"
-          src={warnerbros}
-          alt="Warner Brothers"
-        />
-        <img
-          className="partner-showcase__logo"
-          src={ffa}
-          alt="Film Festival Allaince"
-        />
-        <img
-          className="partner-showcase__logo"
-          src={sonyp}
-          alt="Sony Pictures"
-        />
-        <img
-          className="partner-showcase__logo"
-          src={sidewalkff}
-          alt="Sidewalk Film Festival"
-        />
-        <img
-          className="partner-showcase__logo"
-          src={disneyp}
-          alt="Disney Plus"
-        />
-        <img
-          className="partner-showcase__logo"
-          src={pp}
-          alt="Paramount Pictures"
-        />
-        <img
-          className="partner-showcase__logo"
-          src={tiff}
-          alt="Toronto International Film Festival"
-        />
-        <img
-          className="partner-showcase__logo"
-          src={nff}
-          alt="Nashville Film Festival"
-        />
-        <img
-          className="partner-showcase__logo"
-          src={own}
-          alt="Oprah Winfrey Network"
-        />
-        <img
-          className="partner-showcase__logo"
-          src={laiff}
-          alt="LA International Film Festival"
-        />
-        <img
-          className="partner-showcase__logo"
-          src={sff}
-          alt="Sundance Film Festival"
-        />
-        <img
-          className="partner-showcase__logo"
-          src={tps}
-          alt="Tyler Perry Studios"
-        />
-        <img
-          className="partner-showcase__logo"
-          src={nciff}
-          alt="Niagara Canada International Film Festival"
-        />
+      <div className="partner-showcase__logos-container">
+        <div className="partner-showcase__logos">
+          {logos.concat(logos).map((logo, index) => (
+            <img
+              key={index}
+              className="partner-showcase__logo"
+              src={logo}
+              alt="Partner Logo"
+            />
+          ))}
+        </div>
       </div>
     </section>
   );
 }
+
+// import React from "react";
+// import "./PartnerShowcase.scss";
+
+// import biaff from "../../assets/Logo/Berlin-International-Art-FF.jpeg";
+// import atlantaff from "../../assets/Logo/Atlanta-FF.png";
+// import netflix from "../../assets/Logo/Netflix-logo.png";
+// import nciff from "../../assets/Logo/Niagara-Canada-International-FF.png";
+// import cinemap from "../../assets/Logo/cinema-production-logo.jpg";
+// import whistlerff from "../../assets/Logo/Whistler_film_festivals_logo.png";
+// import warnerbros from "../../assets/Logo/Warner-Bros-logo.png";
+// import ffa from "../../assets/Logo/FF-Alliance.png";
+// import sonyp from "../../assets/Logo/sony-pictures-logo.png";
+// import sidewalkff from "../../assets/Logo/Sidewalk-FF-logo.png";
+// import disneyp from "../../assets/Logo/Disney+.png";
+// import pp from "../../assets/Logo/Paramount-Pictures-logo.png";
+// import tiff from "../../assets/Logo/tiff-logo.png";
+// import nff from "../../assets/Logo/NashFilmRegLogo.jpg";
+// import own from "../../assets/Logo/oprah-winfrey-network-logo.jpg";
+// import laiff from "../../assets/Logo/LA-International-FF-logo.png";
+// import sff from "../../assets/Logo/sundance-film-festival-logo.jpg";
+// import tps from "../../assets/Logo/Tyler-Perry-Studios.jpg";
+
+// export default function PartnerShowcase() {
+//   return (
+//     <section className="partner-showcase">
+//       <h2 className="partner-showcase__title">Our Partners</h2>
+//       <p className="partner-showcase__text">
+//         Studio Board is honored to collaborate with some of the world's most
+//         renowned media companies and festivals, helping to create valuable job
+//         opportunities and foster meaningful networking experiences.
+//       </p>
+//       <div className="partner-showcase__logos">
+//         <img
+//           className="partner-showcase__logo"
+//           src={biaff}
+//           alt="Berlin International Art Film Festival"
+//         />
+//         <img
+//           className="partner-showcase__logo"
+//           src={atlantaff}
+//           alt="Atlanta Film Festival"
+//         />
+//         <img className="partner-showcase__logo" src={netflix} alt="Netflix" />
+//         <img
+//           className="partner-showcase__logo"
+//           src={nciff}
+//           alt="Niagara Canada International Film Festival"
+//         />
+//         <img
+//           className="partner-showcase__logo"
+//           src={cinemap}
+//           alt="Cinema Production"
+//         />
+//         <img
+//           className="partner-showcase__logo"
+//           src={whistlerff}
+//           alt="Whistler Film Festials"
+//         />
+//         <img
+//           className="partner-showcase__logo"
+//           src={warnerbros}
+//           alt="Warner Brothers"
+//         />
+//         <img
+//           className="partner-showcase__logo"
+//           src={ffa}
+//           alt="Film Festival Allaince"
+//         />
+//         <img
+//           className="partner-showcase__logo"
+//           src={sonyp}
+//           alt="Sony Pictures"
+//         />
+//         <img
+//           className="partner-showcase__logo"
+//           src={sidewalkff}
+//           alt="Sidewalk Film Festival"
+//         />
+//         <img
+//           className="partner-showcase__logo"
+//           src={disneyp}
+//           alt="Disney Plus"
+//         />
+//         <img
+//           className="partner-showcase__logo"
+//           src={pp}
+//           alt="Paramount Pictures"
+//         />
+//         <img
+//           className="partner-showcase__logo"
+//           src={tiff}
+//           alt="Toronto International Film Festival"
+//         />
+//         <img
+//           className="partner-showcase__logo"
+//           src={nff}
+//           alt="Nashville Film Festival"
+//         />
+//         <img
+//           className="partner-showcase__logo"
+//           src={own}
+//           alt="Oprah Winfrey Network"
+//         />
+//         <img
+//           className="partner-showcase__logo"
+//           src={laiff}
+//           alt="LA International Film Festival"
+//         />
+//         <img
+//           className="partner-showcase__logo"
+//           src={sff}
+//           alt="Sundance Film Festival"
+//         />
+//         <img
+//           className="partner-showcase__logo"
+//           src={tps}
+//           alt="Tyler Perry Studios"
+//         />
+//         <img
+//           className="partner-showcase__logo"
+//           src={nciff}
+//           alt="Niagara Canada International Film Festival"
+//         />
+//       </div>
+//     </section>
+//   );
+// }
