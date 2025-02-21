2d7cf0f92b62d9aec430710bcc753c471ca28ed3
Febbie Shikongo <feb.shikongo@gmail.com>
2025-02-21 10:02:01 -0500

    updated styling

--- studio-board-frontend/src/pages/JobBoardPage/JobBoardPage.jsx
+++ studio-board-frontend/src/pages/JobBoardPage/JobBoardPage.jsx
@@ -1,13 +1,13 @@
 import "./JobBoardPage.scss";
 import HeroJB from "../../component/HeroJB/HeroJB";
 import JobEventList from "../../component/JobEventList/JobEventList";
-import JobEventCard from "../../component/JobEventCard/JobEventCard";
+// import JobEventCard from "../../component/JobEventCard/JobEventCard";
 
 export default function JobBoard() {
   return (
     <>
       <HeroJB />
-      <JobEventCard />
+      {/* <JobEventCard /> */}
       <JobEventList />
     </>
   );
